e6641af9f3d2c978e3d9b8ee1d600e67e2bc332e
JC Lee <jcleefw@gmail.com>
2020-04-03 16:19:34 +1100

    install use-http to fetch data from mock data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useFetch } from "use-http";
 
 function App() {
+  const { loading, error, data } = useFetch("data.json", {}, []);
+  if (loading) {
+    return <span>...</span>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={data.profile_pic_url}
+          className="App-logo"
+          alt={data.username}
+        />
+        {data.username}
       </header>
+
+      <footer>
+        <img src={logo} className="App-logo" alt="logo" />
+      </footer>
     </div>
   );
 }
